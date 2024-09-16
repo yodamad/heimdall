9e70f4dbbdd75d9cbc4f375ff1129aaf432086ff
yodamad <mvincent.it@gmail.com>
2024-09-16 22:22:57 +0200

    🗽 Check status of git folders

--- cmd/gitdirs.go
+++ cmd/gitdirs.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"heimdall/utils"
@@ -71,25 +72,55 @@ func listGitDirs() {
 	nbIgnoreSlashes := strings.Count(rootDir, "/")
 	nbGitFolders := 0
 
-	filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			// handle possible path err, just in case...
-			return err
-		}
-		if d.IsDir() && (strings.Count(path, string(os.PathSeparator))-nbIgnoreSlashes) > maxDepth {
-			utils.Trace("Skip "+path, true)
-			return fs.SkipDir
-		} else if d.IsDir() {
-			utils.Trace("Inspecting "+path, true)
-			_, err := os.Stat(path + "/.git")
-			if err == nil {
-				utils.Trace("Found a .git folder : "+path, true)
-				nbGitFolders++
-				return fs.SkipDir
+	rootIsGit, _ := checkIsGitDir(rootDir)
+	if !rootIsGit {
+		filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				// handle possible path err, just in case...
+				return err
 			}
-		}
-		// ... process entry
-		return nil
-	})
+			if d.IsDir() && (strings.Count(path, string(os.PathSeparator))-nbIgnoreSlashes) > maxDepth {
+				utils.Trace("Skip "+path, true)
+				return fs.SkipDir
+			} else if d.IsDir() {
+				foundGit, err := checkIsGitDir(path)
+				if err == nil && foundGit {
+					nbGitFolders++
+					return fs.SkipDir
+				}
+			}
+			// ... process entry
+			return nil
+		})
+	}
+
 	utils.Trace("Found "+strconv.Itoa(nbGitFolders)+" folders", false)
+}
+
+func checkIsGitDir(path string) (bool, error) {
+	utils.Trace("Inspecting "+path, true)
+	_, err := os.Stat(path + "/.git")
+	if err == nil {
+		utils.Trace("Found a .git folder : "+path, true)
+
+		checkIfUpToDate(path)
+
+		if err != nil {
+			return false, err
+		}
+
+		return true, nil
+	}
+	return false, err
+}
+
+func checkIfUpToDate(path string) (git.Status, error) {
+	repo, err := git.PlainOpen(path)
+	repo.Fetch(&git.FetchOptions{})
+	w, err := repo.Worktree()
+	s, err := w.Status()
+
+	utils.Trace(path+" is up-to-date ? "+strconv.FormatBool(s.IsClean()), true)
+
+	return s, err
 }
