f5ea2b7772e7168d06f7c842a3280e9eb6f3d719
yodamad <mvincent.it@gmail.com>
2024-09-24 18:15:19 +0200

    ✏️ Add some light doc

--- commons/const.go
+++ commons/const.go
@@ -1,7 +1,16 @@
 package commons
 
+// DEFAULT_FOLDER /* Default folder for git-info search */
 const DEFAULT_FOLDER = "."
+
+// MAX_DEPTH /* Depth of search */
 const MAX_DEPTH = 3
 
+// RootDir /* Which directory to start from */
 var RootDir string
+
+// Verbose /* Verbose log */
 var Verbose bool
+
+// Interactive /* Interactive mode */
+var Interactive bool
